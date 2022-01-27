b02348676c511395beec5e3cdc5e7aa9c8c7f572
Richard Rosenberg <rosenbr73@gmail.com>
2022-01-26 17:03:30 -0800

    moved pic folder

--- src/Map.js
+++ src/Map.js
@@ -2,7 +2,7 @@ function Map() {
     return (
         <div class="hop__main">
             <div class="hop__map">
-                <img src="hopland__react/public/pics/hoplandmap.jpeg" alt="Destination Hopland Map" class="hop__map"></img>
+                <img src="/Users/richardrosenberg/Projects/hopland__react/hopland__react/src/pics/hoplandmap.jpeg" alt="Destination Hopland Map" class="hop__map"></img>
             </div>
         </div>
     )
