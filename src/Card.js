1a234a67bf1baa378328e07019ee83c8781c9bd8
Richard Rosenberg <rosenbr73@gmail.com>
2022-01-26 17:34:34 -0800

    more CSS tinkering

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ function Card() {
             <h4>Hours</h4>
             <h4>Phone Number</h4>
             <p>Blurb</p>
-            <h4>Reso's</h4>
+            <h4>Reso's ?</h4>
             <h4>Directions</h4>
         </div>
     )
