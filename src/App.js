270e2daf5b8789ff9d35001346bc579ca5f3b5e8
Richard Rosenberg <rosenbr73@gmail.com>
2022-02-03 16:09:48 -0800

    added a div for the button component

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
 
   return (
     <div className="App">
-      <Button />
-      <Button />
-      <Button />
+      <div class="hop__button">
+        <Button />
+        <Button />
+        <Button />
+      </div>
       <div class="hop__main">
         <Map />
         <Info />
